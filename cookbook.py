616198e9d88f69afd4565d74b8287064114eb526
Antoine Labard <antoine@labard.fr>
2023-05-22 14:52:57 +0200

    feat: 📝 add the documentation of the script

--- cookbook.py
+++ cookbook.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
+
+"""
+NAME
+    cookbook.py - Generate custom meals.
+
+SYNOPSIS
+    ./cookbook.py [OPTION]...
+
+DESCRIPTION
+    cookbook.py is intended to generate a random menu matching some filters. Those filters can be given as command line
+    arguments, but they also can be given via profiles defines in the script. This script is used to make sure that no
+    human mind is involved in the random generation of menus, as it is bad at randomness and risks promote some recipes
+    over others. The idea is to randomly pick the recipes matching the provided filters using a draw without discount
+    and bet on the equiprobability of each recipe to be chosen in order to statistically select every recipe over time.
+
+CONFIGURATION
+    cookbook.py can take several arguments:
+        - export: create a markdown file containing wikilinks for all the recipes of the cookbook. It can be read using
+        Obsidian (https://obsidian.md).
+        - plan=PLAN: generate a menu following the filters pointed by PLAN (defined in the script)
+        - filter=VALUE: set a custom filter that the recipes need to match. Here is the list of filters with their
+        expected value:
+            - type: "meal"|"ingredient"|"inedible"
+            - opportunity: None|"cheat-meal"|"party"|"pleasure"
+            - lunch: integer
+            - breakfast: integer
+            - snack: integer
+            - appetizer: integer
+"""
+
 import copy
 import os
 import sys
