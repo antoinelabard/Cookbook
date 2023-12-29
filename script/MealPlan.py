a580176d20e9a90e185d00470cc1f68da82c86fd
Antoine Labard <antoine@labard.fr>
2023-12-29 22:01:55 +0100

    👔️ update MealPlan fields types

--- script/MealPlan.py
+++ script/MealPlan.py
@@ -1,11 +1,11 @@
 from script import Recipe
 
 
-class MealPlans:
+class MealPlan:
     def __init__(self,
-                 lunches: list[Recipe],
-                 breakfasts: list[Recipe],
-                 snacks: list[Recipe]):
-        self.lunches: list[Recipe] = lunches
-        self.breakfasts: list[Recipe] = breakfasts
-        self.snacks: list[Recipe] = snacks
+                 lunches=None,
+                 breakfasts=None,
+                 snacks=None):
+        self.lunches: list[Recipe] = lunches or []
+        self.breakfasts: list[Recipe] = breakfasts or []
+        self.snacks: list[Recipe] = snacks or []
